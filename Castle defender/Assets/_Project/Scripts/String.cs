c4395b63fcc4c082e558ea78207d4e318a50be40
Destroliedel22 <Jordiwalet@gmail.com>
2026-09-02 12:58:37 +0200

    String can be pulled back on one axis and moves back

---
+++ Castle defender/Assets/_Project/Scripts/String.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class String : MonoBehaviour
+{
+    [SerializeField] private float minDrawDistance;
+    [SerializeField] private float maxDrawDistance;
+    [SerializeField] private Transform stringRestPoint;
+
+    private Rigidbody rb;
+    private Transform grabbedHand;
+    private Vector3 startPos;
+    private Vector3 drawAxis;
+    private float drawDistance;
+    private float grabOffset;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        startPos = stringRestPoint.position;
+        drawAxis = stringRestPoint.up;
+
+        if (grabbedHand)
+        {
+            Vector3 handDirection = grabbedHand.position - startPos;
+            drawDistance = Vector3.Dot(handDirection, drawAxis) - grabOffset;
+            float clampDistance = Mathf.Clamp(drawDistance, minDrawDistance, maxDrawDistance);
+            Vector3 pos = startPos + drawAxis * clampDistance;
+            rb.MovePosition(pos);
+        }
+        else
+        {
+            rb.MovePosition(startPos);
+        }
+    }
+
+    public void OnGrab(SelectEnterEventArgs args)
+    {
+        grabbedHand = args.interactorObject.transform;
+
+        Vector3 initialHandDirection = grabbedHand.position - startPos;
+        grabOffset = Vector3.Dot(initialHandDirection, drawAxis);
+    }
+
+    public void OnLetGo(SelectExitEventArgs args)
+    {
+        grabbedHand = null;
+    }
+}

1deddd5f3d0e821c97825abb7929db25ab289df5
Destroliedel22 <Jordiwalet@gmail.com>
2026-09-08 10:23:30 +0200

    Enemy climbs up stairs and goes to player to attack

--- Castle defender/Assets/_Project/Scripts/Enemy.cs
+++ Castle defender/Assets/_Project/Scripts/Enemy.cs
@@ -14,18 +14,25 @@ public class Enemy : MonoBehaviour
 
     [HideInInspector] public Transform Target;
 
-    [SerializeField] protected float walkSpeed;
-    [SerializeField] protected float climbTime;
-    [SerializeField] protected float minAttackTimer;
-    [SerializeField] protected float maxAttackTimer;
+    [SerializeField] protected float walkSpeed = 1;
+    [SerializeField] protected float climbTime = 5;
+    [SerializeField] protected float minAttackTimer = 1;
+    [SerializeField] protected float maxAttackTimer = 5;
+
+    [SerializeField] private float walkToClimbDelay = 1;
+    [SerializeField] private float climbToWalkDelay = 2;
 
     protected Animator animator;
     protected float attackTimer;
 
     private EnemyState enemyState;
     private const string WALKING_STATE = "Walking";
+    private const string CLIMBING_STATE = "Climbing";
+    private const string ATTACKING_STATE = "Attack";
+
     private Rigidbody[] rigidbodies;
 
+    private bool hasClimbed;
     private bool isClimbing;
 
     private Coroutine climbRoutine;
@@ -38,42 +45,88 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
-        animator.SetBool(WALKING_STATE, true);
         attackTimer = Random.Range(minAttackTimer, maxAttackTimer);
     }
 
     private void Update()
     {
-        if(enemyState == EnemyState.Walking)
+        switch (enemyState)
         {
-            Vector3 pos = Vector3.MoveTowards(transform.position, Target.transform.position, walkSpeed * Time.deltaTime);
+            case EnemyState.Walking:
+                Walk();
+                if (Vector3.Distance(transform.position, Target.position) < 1f)
+                {
+                    if (!hasClimbed)
+                    {
+                        animator.SetBool(WALKING_STATE, false);
+                        if (walkToClimbDelay > 0f)
+                            walkToClimbDelay -= Time.deltaTime;
+                        else
+                            enemyState = EnemyState.Climbing;
+                    }
+                    else
+                        enemyState = EnemyState.Attacking;
+                }
+                break;
+
+            case EnemyState.Climbing:
+                if (!isClimbing && !hasClimbed)
+                    climbRoutine = StartCoroutine(EnemyClimb());
+
+                if (Vector3.Distance(transform.position, Target.position) < 1f)
+                {
+                    if (isClimbing)
+                    {
+                        animator.SetBool(CLIMBING_STATE, false);
+                        isClimbing = false;
+                        hasClimbed = true;
+
+                        foreach (Rigidbody rb in rigidbodies)
+                            rb.useGravity = true;
+                    }
+
+                    if (climbToWalkDelay > 0f)
+                        climbToWalkDelay -= Time.deltaTime;
+                    else
+                    {
+                        Target = Camera.main.transform.root;
+                        enemyState = EnemyState.Walking;
+                    }
+                }
+                break;
+
+            case EnemyState.Attacking:
+                animator.SetBool(WALKING_STATE, false);
+                Attack();
+                break;
+
+        }
+    }
+
+    private void Walk()
+    {
+        animator.SetBool(WALKING_STATE, true);
+        Vector3 pos = Vector3.MoveTowards(transform.position, Target.transform.position, walkSpeed * Time.deltaTime);
+        if (!hasClimbed)
             pos.y = Terrain.activeTerrain.SampleHeight(pos);
-            transform.position = pos;
-            transform.LookAt(Target);
-            if (Vector3.Distance(transform.position, Target.position) < 1f)
-                enemyState = EnemyState.Climbing;
-        }
-        else if(enemyState == EnemyState.Climbing)
+        transform.position = pos;
+        transform.LookAt(Target);
+    }
+
+    private void Attack()
+    {
+        if (attackTimer > 0f)
+            attackTimer -= Time.deltaTime;
+        else
         {
-            if (isClimbing == false)
-                climbRoutine = StartCoroutine(EnemyClimb());
-        }
-        else if(enemyState == EnemyState.Attacking)
-        {
-            if (attackTimer > 0f)
-                attackTimer -= Time.deltaTime;
-            else
-            {
-                attackTimer = Random.Range(minAttackTimer, maxAttackTimer);
-                animator.SetTrigger("Attack");
-            }
+            attackTimer = Random.Range(minAttackTimer, maxAttackTimer);
+            animator.SetTrigger(ATTACKING_STATE);
         }
     }
 
     IEnumerator EnemyClimb()
     {
         isClimbing = true;
-        enemyState = EnemyState.Climbing;
 
         foreach (Rigidbody rb in rigidbodies)
             rb.useGravity = false;
@@ -83,9 +136,10 @@ public class Enemy : MonoBehaviour
         transform.rotation = Quaternion.Euler(transform.rotation.x + rotationOffset, transform.rotation.y, transform.rotation.z);
 
         animator.SetBool(WALKING_STATE, false);
-        animator.SetTrigger("Climbing");
+        animator.SetBool(CLIMBING_STATE, true);
 
         Transform NewTarget = Target.GetChild(0);
+        Target = NewTarget;
 
         Vector3 startPos = transform.position;
         Vector3 endPos = NewTarget.position;
@@ -113,7 +167,7 @@ public class Enemy : MonoBehaviour
 
         OnDeath?.Invoke(this);
         foreach (Rigidbody rb in rigidbodies)
-            rb.useGravity = false;
+            rb.useGravity = true;
         animator.enabled = false;
         Destroy(this.gameObject, 2);
     }

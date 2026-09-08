33a027eb7b1ebcb84de903bd403b86e27e0a3c84
Destroliedel22 <Jordiwalet@gmail.com>
2026-09-08 17:00:22 +0200

    bow and player respawn on wall when fallen

---
+++ Castle defender/Assets/_Project/Scripts/BowDeathZone.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BowDeathZone : MonoBehaviour
+{
+    [SerializeField] private Transform tpTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bow"))
+        {
+            Rigidbody rb = other.GetComponentInParent<Rigidbody>();
+            rb.linearVelocity = Vector3.zero;
+            rb.position = tpTransform.position;
+            //other.transform.position = tpTransform.position;
+        }
+
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            other.transform.position = tpTransform.position;
+    }
+}

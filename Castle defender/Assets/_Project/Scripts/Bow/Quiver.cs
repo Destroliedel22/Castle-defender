8bb6311c96422db66fb302384a665b897157139a
Destroliedel22 <Jordiwalet@gmail.com>
2026-09-10 14:36:14 +0200

    Quiver always rotates

--- Castle defender/Assets/_Project/Scripts/Bow/Quiver.cs
+++ Castle defender/Assets/_Project/Scripts/Bow/Quiver.cs
@@ -1,15 +1,13 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class ArrowPouch : MonoBehaviour
+public class Quiver : MonoBehaviour
 {
     [SerializeField] private Transform arrowTransform;
     [SerializeField] private GameObject arrowPrefab;
 
     [SerializeField] private float rotateSpeed;
-    [SerializeField] private float rotateDegree = 45;
     [SerializeField] private Vector3 offset = new(0, 0.3f, 0.2f);
 
     private XRGrabInteractable currentArrow;
@@ -22,14 +20,10 @@ public class ArrowPouch : MonoBehaviour
     private void LateUpdate()
     {
         Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
-        float angleDifference = Mathf.DeltaAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y);
 
-        if (Mathf.Abs(angleDifference) > rotateDegree)
-        {
-            Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
-            transform.position = Camera.main.transform.position + (rotation * offset);
-            transform.rotation = rotation;
-        }
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        transform.position = Camera.main.transform.position + (rotation * offset);
+        transform.rotation = rotation;
     }
 
     private void ArrowGrabbed()
@@ -37,7 +31,6 @@ public class ArrowPouch : MonoBehaviour
         if (currentArrow)
         {
             currentArrow.transform.localScale = arrowPrefab.transform.localScale;
-            print(currentArrow.transform.localScale);
             currentArrow.selectEntered.RemoveListener(OnArrowSelected);
         }
 

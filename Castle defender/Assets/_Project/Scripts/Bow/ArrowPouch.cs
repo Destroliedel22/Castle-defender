2d5bb4a669d143fa98a4c0f8af4ba32a8c85ec1f
Destroliedel22 <Jordiwalet@gmail.com>
2026-09-10 14:32:08 +0200

    Quiver changed to 2 hip quivers

--- Castle defender/Assets/_Project/Scripts/Bow/ArrowPouch.cs
+++ Castle defender/Assets/_Project/Scripts/Bow/ArrowPouch.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
@@ -7,6 +8,10 @@ public class ArrowPouch : MonoBehaviour
     [SerializeField] private Transform arrowTransform;
     [SerializeField] private GameObject arrowPrefab;
 
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float rotateDegree = 45;
+    [SerializeField] private Vector3 offset = new(0, 0.3f, 0.2f);
+
     private XRGrabInteractable currentArrow;
 
     private void Start()
@@ -14,12 +19,30 @@ public class ArrowPouch : MonoBehaviour
         ArrowGrabbed();
     }
 
+    private void LateUpdate()
+    {
+        Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
+        float angleDifference = Mathf.DeltaAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y);
+
+        if (Mathf.Abs(angleDifference) > rotateDegree)
+        {
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+            transform.position = Camera.main.transform.position + (rotation * offset);
+            transform.rotation = rotation;
+        }
+    }
+
     private void ArrowGrabbed()
     {
         if (currentArrow)
+        {
+            currentArrow.transform.localScale = arrowPrefab.transform.localScale;
+            print(currentArrow.transform.localScale);
             currentArrow.selectEntered.RemoveListener(OnArrowSelected);
+        }
 
         GameObject clone = Instantiate(arrowPrefab, arrowTransform.position, arrowTransform.rotation, transform);
+        clone.transform.localScale = arrowTransform.localScale;
         currentArrow = clone.GetComponent<XRGrabInteractable>();
         currentArrow.selectEntered.AddListener(OnArrowSelected);
     }
--- Castle defender/Assets/_Project/Scripts/Player.cs
+++ Castle defender/Assets/_Project/Scripts/Player.cs
@@ -7,30 +7,18 @@ public class Player : MonoBehaviour
     public PlayerSettings Settings;
     public int Health;
 
-    [SerializeField] private MeshRenderer fadeOutRend;
-
-    private Material fadeOutMat;
-    private Color color;
-
     private bool gameOverEventCalled;
 
     private void Awake()
     {
         Health = Settings.Health;
-        fadeOutMat = fadeOutRend.material;
-        color = fadeOutMat.color;
     }
 
     private void Update()
     {
         if (Health <= 0)
         {
-            if (color.a < 1)
-            {
-                color.a += 0.01f;
-                fadeOutMat.color = color;
-            }
-            else if(!gameOverEventCalled)
+            if(!gameOverEventCalled)
             {
                 gameOverEventCalled = true;
                 GameOver?.Invoke();
